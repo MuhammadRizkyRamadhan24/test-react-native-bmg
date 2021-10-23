b6cc0da66dc6ba0f4840300d12d977f1842d886e
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-10-23 18:18:56 +0700

    create home screen

---
+++ src/components/CardHome.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  ScrollView,
+  TextInput,
+  ImageBackground,
+  FlatList,
+} from 'react-native';
+
+export default function CardHome(props) {
+  const url = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
+  const item = props.item
+  return (
+    <View>
+      <TouchableOpacity style={styles.wrapperCard}>
+        <Image
+          style={styles.cardImage}
+          source={{
+            uri: `${url}${item.poster_path}`,
+          }}
+        />
+        <Text
+          style={[
+            styles.fontBold,
+            styles.fontSmall,
+            styles.colorLightBlack,
+            styles.titleCard,
+          ]}>
+          {item.title}
+        </Text>
+        <Text
+          style={[
+            styles.fontLight,
+            styles.fontSmall,
+            styles.colorLightBlack,
+            styles.textDate,
+          ]}>
+          {item.release_date}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const win = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  fontBold: {
+    fontFamily: 'OpenSans-Bold',
+  },
+  fontSemiBold: {
+    fontFamily: 'OpenSans-SemiBold',
+  },
+  fontLight: {
+    fontFamily: 'OpenSans-Light',
+  },
+  fontLarge: {
+    fontSize: 38,
+  },
+  fontMedium: {
+    fontSize: 24,
+  },
+  fontNormal: {
+    fontSize: 20,
+  },
+  fontSmall: {
+    fontSize: 14,
+  },
+  colorWhite: {
+    color: '#F4F4F4',
+  },
+  colorLightBlack: {
+    color: '#393534',
+  },
+  colorOrange: {
+    color: '#FF7314',
+  },
+
+  wrapperCard: {
+    width: 140,
+    paddingHorizontal: '2%',
+    alignItems: 'center',
+  },
+  cardImage: {
+    width: '100%',
+    height: 190,
+    resizeMode: 'contain',
+    marginBottom: '5%',
+    borderRadius: 10,
+  },
+  titleCard: {
+    width: '95%',
+    height: 40,
+    marginBottom: '2%',
+  },
+  textDate: {
+    width: '95%',
+  },
+});
---
+++ src/screens/Home.js
@@ -0,0 +1,268 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  ScrollView,
+  TextInput,
+  ImageBackground,
+  FlatList,
+} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import axios from 'axios';
+
+import Navigation from '../components/Navigation';
+import CardHome from '../components/CardHome';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      nowPlaying: '',
+      topRated: '',
+      upComing: '',
+      isLoading: true,
+    };
+  }
+
+  getMovies = async () => {
+    await axios
+      .get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=9b0358ed41781efd00ea52b17a2bb957&language=en-US&page=1',
+      )
+      .then(response => {
+        this.setState({
+          nowPlaying: response.data.results,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    await axios
+      .get(
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=9b0358ed41781efd00ea52b17a2bb957&language=en-US&page=1',
+      )
+      .then(response => {
+        this.setState({
+          upComing: response.data.results,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    await axios
+      .get(
+        'https://api.themoviedb.org/3/movie/top_rated?api_key=9b0358ed41781efd00ea52b17a2bb957&language=en-US&page=1',
+      )
+      .then(response => {
+        this.setState({
+          topRated: response.data.results,
+          isLoading: false,
+        });
+        console.log(this.state.nowPlaying[0]);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  handleChange = val => {
+    this.setState({
+      search: val,
+    });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const url = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
+    return (
+      <>
+        {this.state.isLoading === false ? (
+          <View style={styles.wrapper}>
+            <Navigation func={() => this.props.navigation.openDrawer()} />
+            <View style={styles.wrapperContent}>
+              <ScrollView>
+                <ImageBackground
+                  source={{
+                    uri: `https://searchengineland.com/figz/wp-content/seloads/2015/08/movie-film-video-production-ss-1920-800x450.jpg`,
+                  }}
+                  resizeMode="cover">
+                  <View style={styles.wrapperSearch}>
+                    <Text
+                      style={[
+                        styles.fontBold,
+                        styles.fontLarge,
+                        styles.colorWhite,
+                      ]}>
+                      Welcome.
+                    </Text>
+                    <Text
+                      style={[
+                        styles.fontBold,
+                        styles.fontMedium,
+                        styles.colorWhite,
+                      ]}>
+                      Millions of movies, TV shows and people to discover.
+                      Explore now.
+                    </Text>
+                    <View style={styles.textInput}>
+                      <MaterialIcons
+                        color={'#A3A3A3'}
+                        name="search"
+                        size={32}
+                      />
+                      <TextInput
+                        placeholder="Find film"
+                        onChangeText={this.handleChange}
+                        onSubmitEditing={() =>
+                          this.props.navigation.navigate('Search', {
+                            search: this.state.search,
+                          })
+                        }
+                      />
+                    </View>
+                  </View>
+                </ImageBackground>
+
+                <View style={styles.wrapperMovies}>
+                  <Text
+                    style={[
+                      styles.fontNormal,
+                      styles.fontBold,
+                      styles.colorLightBlack,
+                    ]}>
+                    Now Playing
+                  </Text>
+
+                  <FlatList
+                    style={styles.flatListCard}
+                    showsHorizontalScrollIndicator={false}
+                    horizontal={true}
+                    data={this.state.nowPlaying}
+                    renderItem={({item}) => <CardHome item={item} />}
+                  />
+                </View>
+
+                <View style={styles.wrapperMovies}>
+                  <Text
+                    style={[
+                      styles.fontNormal,
+                      styles.fontBold,
+                      styles.colorLightBlack,
+                    ]}>
+                    Top Rated
+                  </Text>
+
+                  <FlatList
+                    style={styles.flatListCard}
+                    showsHorizontalScrollIndicator={false}
+                    horizontal={true}
+                    data={this.state.topRated}
+                    renderItem={({item}) => <CardHome item={item} />}
+                  />
+                </View>
+
+                <View style={styles.wrapperMovies}>
+                  <Text
+                    style={[
+                      styles.fontNormal,
+                      styles.fontBold,
+                      styles.colorLightBlack,
+                    ]}>
+                    Upcoming
+                  </Text>
+
+                  <FlatList
+                    style={styles.flatListCard}
+                    showsHorizontalScrollIndicator={false}
+                    horizontal={true}
+                    data={this.state.upComing}
+                    renderItem={({item}) => <CardHome item={item} />}
+                  />
+                </View>
+
+                <View style={styles.bottom}/>
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <View style={styles.wrapper}>
+            <Text>Loading</Text>
+          </View>
+        )}
+      </>
+    );
+  }
+}
+
+const win = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  fontBold: {
+    fontFamily: 'OpenSans-Bold',
+  },
+  fontSemiBold: {
+    fontFamily: 'OpenSans-SemiBold',
+  },
+  fontLight: {
+    fontFamily: 'OpenSans-Light',
+  },
+  fontLarge: {
+    fontSize: 38,
+  },
+  fontMedium: {
+    fontSize: 24,
+  },
+  fontNormal: {
+    fontSize: 20,
+  },
+  fontSmall: {
+    fontSize: 14,
+  },
+  colorWhite: {
+    color: '#F4F4F4',
+  },
+  colorLightBlack: {
+    color: '#393534',
+  },
+
+  wrapper: {
+    flex: 1,
+  },
+  wrapperContent: {
+    flex: 1,
+  },
+  wrapperSearch: {
+    padding: '5%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  textInput: {
+    height: 50,
+    backgroundColor: '#dbdbdb',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: '2%',
+    borderRadius: 999,
+    marginVertical: '5%',
+  },
+  wrapperMovies: {
+    padding: '5%',
+  },
+  flatListCard: {
+    width: '100%',
+    paddingTop: '5%',
+  },
+  bottom: {
+      height: 20,
+      width: '100%',
+  }
+});
